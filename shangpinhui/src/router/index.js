693913173d0c51ddf181fe93f6d80a074c0edde8
wandaokang <wandaokang>
2022-08-29 16:26:04 +0800

    重写push和replace

--- shangpinhui/src/router/index.js
+++ shangpinhui/src/router/index.js
@@ -9,10 +9,37 @@ import Search from '@/pages/Search';
 import Login from '@/pages/Login';
 import Register from '@/pages/Register';
 
+// 先把VueRouter 原型对象的 push，先保存一份
+let originPush = VueRouter.prototype.push;
+let originReplace = VueRouter.prototype.replace;
+
+// 重写 push|replace
+// 第一个参数：告诉原来push方法，你往哪里跳转（传递哪些参数）
+// 第二个参数：成功的回调
+// 第三个参数：失败的回调
+
+// call || apply 区别：
+// 相同点：都可以调用函数一次，都可以篡改函数的上下文一次
+// 不同点：call 与 apply 传递参数：call 传递参数用逗号隔开，apply方法执行，传递数组
+VueRouter.prototype.push = function (location, resolve, reject) {
+  if (resolve && reject) {
+    originPush.call(this, location, resolve, reject);
+  } else {
+    originPush.call(this, location, () => {}, () => {});
+  }
+};
+
+VueRouter.prototype.replace = function (location, resolve, reject) {
+  if (resolve && reject) {
+    originReplace.call(this, location, resolve, reject);
+  } else {
+    originReplace.call(this, location, () => {}, () => {});
+  }
+};
+
 // 配置路由
 export default new VueRouter({
-  routes: [
-    {
+  routes: [{
       path: "/home",
       component: Home,
       meta: {
