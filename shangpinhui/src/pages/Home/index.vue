2cebea4f6ff42ca3560dd92f0a476a7f37ba9d92
wandaokang <wandaokang>
2022-08-30 16:28:02 +0800

    Home页面组件拆分

--- shangpinhui/src/pages/Home/index.vue
+++ shangpinhui/src/pages/Home/index.vue
@@ -3,12 +3,23 @@
     <!-- 三级联动全局组件：三级联动已经注册为全局组件，因此不需要再引入 -->
     <TypeNav />
 
+    <!--列表-->
     <ListContainer />
 
     <!-- 今日推荐 -->
     <Recommend />
 
+    <!-- 商品排行 -->
     <Rank />
+
+    <!-- 猜你喜欢 -->
+    <Like />
+
+    <!-- 楼层 -->
+    <Floor />
+
+    <!-- 商标 -->
+    <Brand />
   </div>
 </template>
 
@@ -16,6 +27,9 @@
 import ListContainer from "@/pages/Home/ListContainer";
 import Recommend from "@/pages/Home/Recommend";
 import Rank from "@/pages/Home/Rank";
+import Like from "@/pages/Home/Like";
+import Floor from "@/pages/Home/Floor";
+import Brand from "@/pages/Home/Brand";
 
 export default {
   name: "Home",
@@ -23,6 +37,9 @@ export default {
     ListContainer,
     Recommend,
     Rank,
+    Like,
+    Floor,
+    Brand,
   },
 };
 </script>
