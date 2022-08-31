ea5b06aa577ab8be2e570ad0778ddb8c92f7538f
wandaokang <wandaokang>
2022-08-31 16:20:18 +0800

    进度条

--- shangpinhui/src/api/request.js
+++ shangpinhui/src/api/request.js
@@ -1,5 +1,9 @@
 // 对于 axios 进行二次封装
 import axios from "axios";
+// 引入进度条
+import nprogress from 'nprogress';
+// 引入进度条样式
+import "nprogress/nprogress.css"
 
 // 1: 利用 axios 对象的方法create，去创建一个 axios 实例
 // 2：request 就是 axios，只不过稍微配置一下
@@ -13,6 +17,9 @@ const requests = axios.create({
 
 // 请求拦截器：在发请求之前，请求拦截器可以检测到，可以在请求发出去之前做一些事情
 requests.interceptors.request.use((config) => {
+  // 进度条开始动
+  nprogress.start();
+
   // config：配置对象，对象里面有一个属性很重要，headers请求头
   return config;
 });
@@ -20,6 +27,10 @@ requests.interceptors.request.use((config) => {
 // 响应拦截器
 requests.interceptors.response.use((res) => {
   // 成功的回调函数：服务器相应数据回来以后，响应拦截器可以检测到，可以做一些事情
+
+  // 进度条结束
+  nprogress.done();
+
   return res.data;
 }, (error) => {
   // 响应失败的回调函数
