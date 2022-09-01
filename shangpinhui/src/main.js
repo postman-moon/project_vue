8833dcbd5ba9e271e8331b863b39e8d16b7e23d3
wandaokang <wandaokang>
2022-09-01 15:46:54 +0800

    Vuex

--- shangpinhui/src/main.js
+++ shangpinhui/src/main.js
@@ -8,6 +8,8 @@ Vue.component(TypeNav.name, TypeNav)
 Vue.config.productionTip = false
 // 引入路由
 import router from "@/router";
+// 引入仓库
+import store from "@/store";
 // 测试
 import {
   reqCategoryList
@@ -18,4 +20,5 @@ reqCategoryList();
 new Vue({
   render: h => h(App),
   router,
+  store,
 }).$mount('#app')
