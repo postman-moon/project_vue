bfa41d1bc2411f1a1c005c2bc83a9a86bb358aaa
wandaokang <wandaokang>
2022-09-01 17:39:34 +0800

    三级分类数据展示

--- shangpinhui/src/main.js
+++ shangpinhui/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-import TypeNav from '@/pages/Home/TypeNav';
+import TypeNav from '@/components/TypeNav';
 
 Vue.component(TypeNav.name, TypeNav)
 
@@ -10,12 +10,6 @@ Vue.config.productionTip = false
 import router from "@/router";
 // 引入仓库
 import store from "@/store";
-// 测试
-import {
-  reqCategoryList
-} from '@/api';
-
-reqCategoryList();
 
 new Vue({
   render: h => h(App),
--- shangpinhui/src/store/home/index.js
+++ shangpinhui/src/store/home/index.js
@@ -1,11 +1,31 @@
+import {
+  reqCategoryList
+} from '@/api';
+
 // state:仓库存储数据的地方
-const state = {};
+const state = {
+  // state 中数据默认初始值别瞎写，服务器返回对象，服务器返回数组【根据接口返回值初始化】
+  categoryList: [],
+};
 
 // mutations: 修改state的唯一手段
-const mutations = {};
+const mutations = {
+  CATEGORYLIST(state, categoryList) {
+    state.categoryList = categoryList;
+  }
+};
 
 // action: 处理action，可以书写自己的业务逻辑，也可以处理异步
-const actions = {};
+const actions = {
+  async categoryList({
+    commit
+  }) {
+    let result = await reqCategoryList();
+    if (result.code === 200) {
+      commit('CATEGORYLIST', result.data);
+    }
+  }
+};
 
 // getters: 理解为计算属性，用于简化仓库数据，让组件获取仓库的数据更加方便
 const getters = {};
